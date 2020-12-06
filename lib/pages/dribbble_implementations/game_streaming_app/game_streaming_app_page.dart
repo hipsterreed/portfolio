b440e732d4a3ae07523abd4dd77ba461fda83658
Jimmy Arroyo <jamesarroyo5@gmail.com>
2020-12-06 13:50:06 -0700

    Created header with two buttons for game streaming app

---
+++ lib/pages/dribbble_implementations/game_streaming_app/game_streaming_app_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class GameStreamingApp extends StatefulWidget {
+  GameStreamingApp({Key key}) : super(key: key);
+
+  @override
+  _GameStreamingAppState createState() => _GameStreamingAppState();
+}
+
+class _GameStreamingAppState extends State<GameStreamingApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(children: [
+            _buildHeaderBar(),
+          ]),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeaderBar() {
+    double containerSize = 55.0;
+
+    return Row(
+      children: [
+        _buildLeadingHeaderButton(containerSize),
+        Spacer(),
+        _buildTrailingHeaderButton(containerSize),
+      ],
+    );
+  }
+
+  Widget _buildLeadingHeaderButton(double containerSize) {
+    double containerPadding = 13.0;
+    double circleIconSize = 8.0;
+    Radius borderRadius = Radius.circular(15.0);
+    return InkWell(
+      child: Container(
+        width: containerSize,
+        height: containerSize,
+        padding: EdgeInsets.all(containerPadding),
+        decoration: BoxDecoration(
+          color: const Color(0xffe6e3f5),
+          borderRadius: BorderRadius.only(topLeft: borderRadius, topRight: borderRadius, bottomRight: borderRadius),
+        ),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: List.generate(4, (index) => Icon(Icons.circle, size: circleIconSize)),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTrailingHeaderButton(double containerSize) {
+    Radius borderRadius = Radius.circular(15.0);
+    return InkWell(
+      child: Container(
+          width: containerSize,
+          height: containerSize,
+          decoration: BoxDecoration(
+            color: const Color(0xffc9e088),
+            borderRadius:
+                BorderRadius.only(bottomLeft: borderRadius, topRight: borderRadius, bottomRight: borderRadius),
+          ),
+          child: Image(image: AssetImage('assets/images/dribbble_game_streaming_app.png'))),
+    );
+  }
+}
